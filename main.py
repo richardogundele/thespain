54cd46a9655651e56d33746c6e9f7b8e9e71c79b
Richard Ogundele <63150179+richardogundele@users.noreply.github.com>
2023-09-12 04:48:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.add_middleware(
                    )
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"ThespAIn": "/text /speech"}
            
 #get history
 @app.post("/history")
