0f2c8857a1ed61e141846c1e7aea5c89a6b6e8a2
Richard Ogundele <ogundelerichard27@gmail.com>
2023-10-05 11:52:37 +0100

    code

--- apirequests.py
+++ apirequests.py
@@ -1,11 +1,16 @@
 import requests, os, openai
 
+''' API REQUEST FROM OPENAI GPT 4'''
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
 
+
 character = "You are a thespian with over 20 years' experience in acting, script analysis, entertainment, content creator, emotional, character development, collaboration, performance, auditions, research and training."
+
 def text_to_text_response(prompt):
+  # Function used to call the GPT 4 API and returns the result
     system = {"role": "system", "content": character}
     user = {"role":"user", "content":prompt}
     try:
@@ -15,32 +20,4 @@ def text_to_text_response(prompt):
         return completion
     except Exception as e: 
         return ("Failed to get text response from GPT4 API")
-        
-# Eleven Labs -- Convert text to speech
-def convert_text_to_speech(result):
-  body = {
-    "text": result,
-    "voice_settings": {
-        "stability": 0,
-        "similarity_boost": 0
-    }
-  }
-  voice_shaun = "mTSvIrm2hmcnOvb21nW2"
-  voice_rachel = "21m00Tcm4TlvDq8ikWAM"
-  voice_antoni = "ErXwobaYiN019PkySvjV"
 
-  # Construct request headers and url
-  headers = { "xi-api-key": ELEVEN_LABS_API_KEY, "Content-Type": "application/json", "accept": "audio/mpeg" }
-  endpoint = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_rachel}"
-
-  try:
-    response = requests.post(endpoint, json=body, headers=headers)
-  except Exception as e:
-     return
-
-  if response.status_code == 200:
-      # with open("output.wav", "wb") as f:
-      #     f.write(audio_data)
-      return response.content
-  else:
-    return
--- mongo.py
+++ mongo.py
@@ -1,27 +1,13 @@
 import certifi, pprint, os
 from pymongo import MongoClient
 
+'''MONGODB DATABASE - to store user emails, conversations of users'''
+
 password = os.getenv("mongopassword")
 
 mongo_uri = f"mongodb+srv://richardogundele:{password}@cluster0.jfqn1lj.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(mongo_uri, tlsCAFile=certifi.where())
 
-# Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
-
+#Get or create database with name 'chatapp' to store the whole conversation and emails
 db = client.get_database("chatapp")  
-
-# client_info = {
-#     "email":"lucianorichard@gmail.com",
-#     "prompt": "where can i find a story",
-#     "completion": "you can find a story in different places"
-# }
-
-# client.chatapp.data.insert_one(client_info)
-# d = db.data.find().next()
-# pprint.pprint(d)
